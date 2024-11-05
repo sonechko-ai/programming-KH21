4e9dedac8ba5ed0fb6c14a9c523a12e243dafd04
Olia <olastasuk371@gmail.com>
2024-11-05 14:51:37 +0200

    code2

---
+++ 2/script.py
@@ -0,0 +1,11 @@
+while True :
+    msg=input("please input a msg")
+    if msg=="добрий день":
+      print("добрий день")
+    elif msg=="слава україна":
+      print("героям слава")
+    elif msg=="hello":
+      print("hello")
+    elif msg=="pa pa ":
+      print("the eng")
+      break
